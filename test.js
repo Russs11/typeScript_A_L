227fb8973761b1282ca37a1a6e0a9472de7e76c7
Russs <russs.khalilov@gmail.com>
2023-07-22 22:28:56 +0300

    h_22.07.2023

---
+++ test.js
@@ -0,0 +1,13 @@
+"use strict";
+// const skills:string[] = ['dev', 'devops']
+// for(const item of skills){
+//     console.log(item.toLocaleUpperCase());
+// }
+// const res = skills
+// .filter((s:string)=> s !== 'testing')
+// .map((s:string) => s + '!!!')
+// .reduce((a, b) => a + b);
+// console.log(res);
+const typedArr = [1, 'wewew'];
+const newArr = typedArr;
+console.log(newArr);
---
+++ test.ts
@@ -0,0 +1,19 @@
+// const skills:string[] = ['dev', 'devops']
+
+// for(const item of skills){
+//     console.log(item.toLocaleUpperCase());
+// }
+
+// const res = skills
+// .filter((s:string)=> s !== 'testing')
+// .map((s:string) => s + '!!!')
+// .reduce((a, b) => a + b);
+
+// console.log(res);
+
+const typedArr: readonly[number, string] = [1, 'wewew'];
+
+const newArr = typedArr
+
+console.log(newArr);
+
